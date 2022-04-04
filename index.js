edd1a11b658d8f8af3ea769a1052344b7af8781d
Khadijah Zunun <halqigularip@gmail.com>
2022-04-04 16:46:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,19 @@
 
 $('#CV_img').mouseover(function(){
   $('#CV_img').animate({'width':"350px"});
-  $('.me-coding').css("color","purple").animate({'font-size':"1.8rem"});
 })
 $('.skill').mouseover(function(){
   $('.skill').animate({'width': '80%'});
   $('.skill').animate({'font-size':"1.4rem"});
 })
-  $('.i').mouseover(function(){
-    $('.i').animate({"font-size":"6rem"});
-  });
+  
   $('.work-exp').mouseover(function(){
-    $('.work-exp').text("I was a researcher in biostatistics and bioinformatics in my previous jobs.").css("color","grey").animate({'font-size':"3rem"});
+    $('.work-exp').text("I was a researcher in biostatistics and bioinformatics in my previous jobs.").css("color","grey");
   });
 
   $(".mycontr").mouseover(function(){
     $('.mycontr').css("color","purple")
-    $(".mycontrtxt").css("color","blue").animate({'font-size':"1.8rem"});
+    $(".mycontrtxt").css("color","blue");
   });
 
 $('.mylogoimg').mouseover(function(){
@@ -26,12 +23,12 @@ $('.mylogoimg').mouseover(function(){
 });
 $('.uniba').mouseover(function(){
   $('.uniba').animate({width:"350px"});
-  $('.unibatxt').css("color","purple","slow").animate({'font-size':"1.8rem"});
+  $('.unibatxt').css("color","purple","slow");
 
 });
 $('.unicam').mouseover(function(){
   $('.unicam').animate({width:"400px"});
-  $('.unicamtxt').css("color","purple").animate({'font-size':"1.8rem"});
+  $('.unicamtxt').css("color","purple");
 });
 $('.hobby').mouseover(function(){
   $('.hobby').animate({'margin-bottom':"50px"});
