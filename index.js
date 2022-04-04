f2207a6501e59c7a621a7561e95a72d48785cb3a
Khadijah Zunun <halqigularip@gmail.com>
2022-04-04 16:38:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 
 
 $('#CV_img').mouseover(function(){
-  $('#CV_img').animate({'width':"350px"},"slow");
-  $('.me-coding').css("color","purple").animate({'font-size':"1.8rem"},"slow");
+  $('#CV_img').animate({'width':"350px"});
+  $('.me-coding').css("color","purple").animate({'font-size':"1.8rem"});
 })
 $('.skill').mouseover(function(){
-  $('.skill').animate({'width': '80%'},"slow");
+  $('.skill').animate({'width': '80%'});
   $('.skill').animate({'font-size':"1.4rem"});
 })
   $('.i').mouseover(function(){
-    $('.i').animate({"font-size":"6rem"},"slow");
+    $('.i').animate({"font-size":"6rem"});
   });
   $('.work-exp').mouseover(function(){
-    $('.work-exp').text("I was a researcher in biostatistics and bioinformatics in my previous jobs.").css("color","grey").animate({'font-size':"3rem"},"slow");
+    $('.work-exp').text("I was a researcher in biostatistics and bioinformatics in my previous jobs.").css("color","grey").animate({'font-size':"3rem"});
   });
 
   $(".mycontr").mouseover(function(){
     $('.mycontr').css("color","purple")
-    $(".mycontrtxt").css("color","blue").animate({'font-size':"1.8rem"},"slow");
+    $(".mycontrtxt").css("color","blue").animate({'font-size':"1.8rem"});
   });
 
 $('.mylogoimg').mouseover(function(){
-  $('.mylogoimg').animate({width:"320px"}, "slow");
+  $('.mylogoimg').animate({width:"320px"});
 
 });
 $('.uniba').mouseover(function(){
   $('.uniba').animate({width:"350px"});
-  $('.unibatxt').css("color","purple","slow").animate({'font-size':"1.8rem"},"slow");
+  $('.unibatxt').css("color","purple","slow").animate({'font-size':"1.8rem"});
 
 });
 $('.unicam').mouseover(function(){
-  $('.unicam').animate({width:"400px"}, "slow");
-  $('.unicamtxt').css("color","purple").animate({'font-size':"1.8rem"},"slow");
+  $('.unicam').animate({width:"400px"});
+  $('.unicamtxt').css("color","purple").animate({'font-size':"1.8rem"});
 });
 $('.hobby').mouseover(function(){
-  $('.hobby').animate({'margin-bottom':"50px"},"slow");
+  $('.hobby').animate({'margin-bottom':"50px"});
 });
